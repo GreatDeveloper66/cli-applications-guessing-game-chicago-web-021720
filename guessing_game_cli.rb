96796a772d88c80900bcb5409433300506b8efc3
cerebral-caller-0985 <ashaffe@gmail.com>
2020-02-02 21:54:58 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,6 @@ def run_guessing_game
   magicnumber = rand(6) + 1
   binding.pry
   puts "Guess a number between 1 and 6"
-
   userguess = gets.chomp
   puts userguess == magicnumber ? "You guessed the correct number" : userguess == "exit" ? "Goodbye!" : "Sorry! Th computer guessed #{magicnumber}"
 end
