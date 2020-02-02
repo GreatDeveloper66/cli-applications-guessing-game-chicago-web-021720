4f90963a6114cd2f67d25276b1116441b5d84791
cerebral-caller-0985 <ashaffe@gmail.com>
2020-02-02 21:49:45 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,8 @@
 # Code your solution here!
+def run_guessing_game
+  magicnumber = rand(6) + 1
+  puts "Guess a number between 1 and 6"
+  userguess = inputs.chomp
+  userguess == magicnumber ? "You guessed the correct number" : userguess == "exit" ? "Goodbye!" : "Sorry! Th computer guessed #{magicnumber}"
+   
+end
